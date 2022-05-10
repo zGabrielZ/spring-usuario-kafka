83fb8e16f8714bdea0baf7fea5c322b16bfce267
Gabriel Ferreira <ferreiragabriel2612@gmail.com>
2022-05-09 21:54:29 -0300

    Projeto Criado

---
+++ src/main/java/com/gabrielferreira/springusuariokafka/SpringUsuarioKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.springusuariokafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUsuarioKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUsuarioKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabrielferreira/springusuariokafka/SpringUsuarioKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielferreira.springusuariokafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUsuarioKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
